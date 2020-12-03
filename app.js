cbca12d50153009320c4e895a66e7858f54df128
DiegoG1 <diegog1proyects@gmail.com>
2020-12-03 17:06:15 -0430

    Version 1.3

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ document.write("<p>Here you can contact me and / or see the projects in order to
 document.write("<p>Email:Diegom.gcalovi@gmail.com");
 
 document.write("<p>Whatsapp:04124341542</p>");
+
+document.write("<p>Twitter</p>");
+
+// Agregado por DiegoG1 //
+// Razon //
+// esta version esta obsoleta debido a la actualizacion de las imagenes tiende a bugearse el canvas y  no deja de funcionar //
+// hay que hacer un nuevo analisis al codigo clave y fuente del codigo de las imagenes para evitar este error //
+// version base elaborada y escrita por DiegoG1 //
