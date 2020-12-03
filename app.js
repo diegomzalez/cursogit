3d91569cbae1c47ad5cf6743785788188f9629e4
DiegoG1 <diegog1proyects@gmail.com>
2020-12-03 13:23:53 -0430

    version 1

---
+++ app.js
@@ -0,0 +1,7 @@
+document.write("<p>Welcome to Page personal DiegoG1</p>");
+
+document.write("<p>Here you can contact me and / or see the projects in order to elaborate and send me to develop in depth</p>")
+
+document.write("<p>Email:Diegom.gcalovi@gmail.com");
+
+document.write("<p>Whatsapp:04124341542</p>");
