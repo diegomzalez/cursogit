841b7ebae4075ffd522b5ab00e1bc16bf0dfeb4e
DiegoG1 <diegog1proyects@gmail.com>
2020-12-04 20:25:04 -0430

    version de prueba para subirlo a github

--- app.js
+++ app.js
@@ -7,11 +7,3 @@ document.write("<p>Email:Diegom.gcalovi@gmail.com");
 document.write("<p>Whatsapp:04124341542</p>");
 
 document.write("<p>Twitter</p>");
-
-// Agregado por DiegoG1 //
-// Razon //
-// esta version esta obsoleta debido a la actualizacion de las imagenes tiende a bugearse el canvas y  no deja de funcionar //
-// hay que hacer un nuevo analisis al codigo clave y fuente del codigo de las imagenes para evitar este error //
-// version base elaborada y escrita por DiegoG1 //
-// ndanfhehdolmafn ebcetyenebe dgbw dggw d wfbw wbeg ebdfdbegheheh . eghendgnbd
-adadsdawdasdadwadwadwasdwadawdwad
